7a87182f7fd7109ec7beaa213e2695be508ee1fc
Rium <kinglessqueenrium@gmail.com>
2023-07-25 06:30:33 +0200

    chore: translated into english, changed icon filename.

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.history.onVisited.addListener(url => {
     if (url.visitCount > 1) {
         const options = {
-            title: "Attenzione!",
-            message: "Hai visitato questa pagina più di una volta!",
-            iconUrl: "./hello_extension.png",
+            title: "Warning!",
+            message: "You have already visited this page.",
+            iconUrl: "./extension_icon.png",
             type: "basic"
         };
         chrome.notifications.create(null, options);
